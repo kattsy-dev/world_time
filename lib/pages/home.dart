19098340d5012c5ca93a79e605f0d537288232ac
mrnotepad <krislane.santillan@gmail.com>
2024-12-27 03:15:38 +0800

    Loaders and Spinners

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/loading.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -17,47 +18,44 @@ class _HomeState extends State<Home> {
     print(data);
 
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: data!.isNotEmpty
-                  ? Column(
-                      mainAxisAlignment:
-                          MainAxisAlignment.center, // Center the content
-                      children: [
-                        TextButton.icon(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/location');
-                          },
-                          icon: const Icon(Icons.edit_location),
-                          label: const Text("Edit Location"),
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(data!['location'] ?? 'No Location Found',
-                                style: const TextStyle(
-                                  fontSize: 28.0,
-                                  letterSpacing: 2,
-                                )),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        Text(
-                          data!['time'] ?? 'No Location Found',
-                          style: const TextStyle(fontSize: 66.0),
-                        ),
-                      ],
-                    )
-                  : const Center(
-                      child: CircularProgressIndicator(),
-                    ))),
-      //body: Text("Home Screen"),
-    );
+        body: SafeArea(
+            child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: data!.isNotEmpty
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Center the content
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: const Icon(Icons.edit_location),
+                  label: const Text("Edit Location"),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(data!['location'] ?? 'No Location Found',
+                        style: const TextStyle(
+                          fontSize: 28.0,
+                          letterSpacing: 2,
+                        )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data!['time'] ?? 'No Location Found',
+                  style: const TextStyle(fontSize: 66.0),
+                ),
+              ],
+            )
+          : const Center(child: Loading()),
+    )));
+    //body: Text("Home Screen"),
   }
 }
